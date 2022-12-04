0cf1a806749f4ad34b9aa91e4eec6092e260e274
Noah Cowan <nsncowan@gmail.com>
2022-12-04 15:51:54 -0800

    WIP: add user interface logic.
    Co-authored-by: Dani Steely <danisteelyart@gmail>
    Co-authored by: Carl Sustrich <carl.sustrich@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -27,20 +27,70 @@ Player.prototype.turnLogic = function(action) {
   }
 };
 
+Player.prototype.rollDice = function() {
+  this.activePlayer = true;
+  let roll = diceRoll();
+  if (roll === 1) {
+    this.turnScore = 0;
+    this.activePlayer = false;
+    return roll;
+  } else {
+    this.turnScore += roll;
+    return roll;
+  }
+};
 
+Player.prototype.endTurn = function() {
+  this.totalScore += this.turnScore;
+  this.activePlayer = false;
+  return this.totalScore;
+}
 
 
 //UI
 
+let player1 = new Player(0, 0, true);
+let player2 = new Player(0, 0, false);
+
+function getActiveUser() {
+  let activeUser;
+  if (player1.activePlayer = true) {
+    activeUser = player1;
+  }
+  else {
+    activeUser = player2;
+  }
+  return activeUser;
+}
+
+function pigDice () {
+  let currentUser = getActiveUser();
+  let rollBtn = document.getElementById("rollDice");
+  let passBtn = document.getElementById("passDice");
+  rollBtn.addEventListener('click', currentUser.prototype.rollDice);
+  passBtn.addEventListener('click', currentUser.prototype.endTurn);
+}
+
+
+/*
 function pigDice () {
   let player1 = new Player(0, 0, true);
   let player2 = new Player(0, 0, false);
+  //let actions = ["roll", "pass"]
+  let rollBtn = document.getElementById("rollDice");
+  let passBtn = document.getElementById("passDice");
+  let playerChoice = rollBtn || passBtn
+  if (player1.activePlayer === "true") {
+    player1.turnLogic(playerChoice);
+  }
   
 }
 
 
+
 function displayScores() {
-  document.querySelector("p#")
+  document.querySelector("p#player1TotalScore").innerText= 
+  document.querySelector("p#player2TotalScore").innerText=
 }
  
 window.addEventListener("load", function (){
@@ -49,37 +99,32 @@ window.addEventListener("load", function (){
 
 
 
-/*
-Player.prototype.roll = function() {
-  let roll = diceRoll();
-  if (roll === 1) {
-      Player.turnScore = 0 
-      Player.activePlayer = false;
-    } else {
-      Player.turnScore += roll;
+
+const player = {
+  currentChoice: null
+}
+const computer = {
+  currentChoice: null
+}
+const choices = ["Rock", "Paper", "Scissors"];
+
+player.currentChoice = choices[2];
+var rockBtn = document.getElementById('rock');
+var paprBtn = document.getElementById('paper');
+var scirBtn = document.getElementById('scissors');
+
+if(rockBtn || paprBtn || scirBtn) {
+    rockBtn.addEventListener('click', updateVar);
+    paprBtn.addEventListener('click', updateVar);
+    scirBtn.addEventListener('click', updateVar);
+}
+function updateVar(event){
+    if(event.target.id == 'rock'){
+        player.currentChoice = choices[0];
+    } else if (event.target.id == 'paper'){
+        player.currentChoice = choices[1];
+    } else if (event.target.id == 'scissors'){
+        player.currentChoice = choices[2];
     }
-  } else {
-    Player.totalScore += Player.turnScore;
-    Player.turnScore = 0;
-    Player.activePlayer = false;
-  }
+    console.log(player.currentChoice);
 }
-
-Player.prototype
-
-
-
-
-show/hide which players turn based on activeplayer status
-
-function pigDice(){
-  create variables for roll
-  create variable for pass
-  create variable for player 1 score display
-  create variable for player 2 score display
-  identify active player
-  run turnLogic for active player
-}
-
-
-*/
